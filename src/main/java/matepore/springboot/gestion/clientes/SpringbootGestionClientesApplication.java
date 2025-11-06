ff7793e5095238a0b251219e55a3ebc3a6a5c0df
Mateo Calcagno <mateo55_malunies@hotmail.com>
2025-11-06 18:25:48 -0300

    Base del proyecto
    Los archivos base del proyecto, nada interesante.

---
+++ src/main/java/matepore/springboot/gestion/clientes/SpringbootGestionClientesApplication.java
@@ -0,0 +1,13 @@
+package matepore.springboot.gestion.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGestionClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGestionClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/matepore/springboot/gestion/clientes/SpringbootGestionClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package matepore.springboot.gestion.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGestionClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
